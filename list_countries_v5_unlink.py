ebe93adcecb84ea697a0947dbf428190af98407b
morrillo <gustavo.orrillo@gmail.com>
2020-07-20 17:09:30 -0300

    Clase i2t

---
+++ list_countries_v5_unlink.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import xmlrpclib
+
+username = 'admin' #the user
+pwd = 'demo_ar'      #the password of the user
+dbname = 'demo_ar'    #the database
+
+## Get the uid
+sock_common = xmlrpclib.ServerProxy ('http://capacitacion:8069/xmlrpc/common')
+uid = sock_common.login(dbname, username, pwd)
+#
+##replace localhost with the address of the server
+sock = xmlrpclib.ServerProxy('http://capacitacion:8069/xmlrpc/object')
+
+currency_id = sock.execute(dbname,uid,pwd,'res.currency','search',[('name','=','USD')])
+if not currency_id:
+	print("No existe el USD")
+	exit(4)
+
+country_ids = sock.execute(dbname,uid,pwd,'res.country','search',[('name','=','Rodrigombia')])
+#country_ids = sock.execute(dbname,uid,pwd,'res.country','search',['|',('name','=','Rodrigombia'),('currency_id.name','=','USD')])
+#country_ids = sock.execute(dbname,uid,pwd,'res.country','search',[('name','=','Rodrigombia'),('currency_id','=',currency_id[0])])
+#country_ids = sock.execute(dbname,uid,pwd,'res.country','search',['!',('currency_id','=',currency_id[0])])
+
+if country_ids:
+	return_id = sock.execute(dbname,uid,pwd,'res.country','unlink',country_ids)
+	print return_id
+
